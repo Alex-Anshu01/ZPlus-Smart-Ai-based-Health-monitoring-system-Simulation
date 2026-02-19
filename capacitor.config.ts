b46fbe28970b18fd3d1fb541d187279ea694e4fd
Anshu Kumar <kanshu0023@gmail.com>
2026-02-19 22:29:47 +0530

    Initial project setup with core and configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zplus.health',
+  appName: 'Z+',
+  webDir: 'dist'
+};
+
+export default config;
